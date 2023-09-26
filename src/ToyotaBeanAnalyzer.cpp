cac8e32d20da75f22c8fb1ed97fd0c1832d6a817
Matthew Kennedy <matthewkennedy@outlook.com>
2023-09-25 23:27:55 -0700

    blind swing at it

--- src/ToyotaBeanAnalyzer.cpp
+++ src/ToyotaBeanAnalyzer.cpp
@@ -22,52 +22,159 @@ void ToyotaBeanAnalyzer::SetupResults()
     mResults->AddChannelBubblesWillAppearOn(mSettings->mInputChannel);
 }
 
+class BitUnstuffer
+{
+public:
+    BitUnstuffer(
+        AnalyzerChannelData* data, std::auto_ptr<ToyotaBeanAnalyzerResults> results, Channel channel, U32 bitTime)
+        : mData(data), mResults(results), mBitTime(bitTime), mChannel(channel)
+    {
+    }
+
+    bool ReadBit()
+    {
+        mData->Advance(mBitTime);
+        // This is a data bit, mark it with the dot
+        mResults->AddMarker(mData->GetSampleNumber(), AnalyzerResults::Dot, mChannel);
+        bool bit = mData->GetBitState() == BIT_HIGH;
+
+        bool expectStuff = false;
+
+        // This is a natural transition, reset stuffing
+        if (bit != mLastBit)
+        {
+            mLastBit = bit;
+            mBitsInSequence = 1;
+        }
+        else
+        {
+            mBitsInSequence++;
+
+            if (mBitsInSequence == 5)
+            {
+                // This is 5 in a row, next bit should be a stuff!
+                expectStuff = true;
+            }
+        }
+
+        if (expectStuff)
+        {
+            mData->Advance(mBitTime);
+            // mark the stuffed bit with the square
+            mResults->AddMarker(mData->GetSampleNumber(), AnalyzerResults::Square, mChannel);
+            bool stuffBit = mData->GetBitState() == BIT_HIGH;
+
+            // Stuff bit should be opposite the last data bit
+            // If not, it's a bit stuffing error
+            if (stuffBit == bit)
+            {
+                throw "bit stuff err";
+            }
+            else
+            {
+                // The last bit was now the stuffed bit, reset state to that
+                mLastBit = stuffBit;
+                mBitsInSequence = 1;
+            }
+        }
+
+        return bit;
+    }
+
+    uint8_t ReadBits(size_t bits)
+    {
+        uint8_t result = 0;
+
+        // TODO: is it MSB first or LSB first?
+        for (size_t i = 0; i < bits; i++)
+        {
+            result = result << 1;
+            result |= ReadBit();
+        }
+
+        return result;
+    }
+
+    uint8_t ReadByte()
+    {
+        return ReadBits(8);
+    }
+
+private:
+    AnalyzerChannelData* mData;
+    std::auto_ptr<ToyotaBeanAnalyzerResults> mResults;
+    Channel mChannel;
+    U32 mBitTime;
+
+    // We start at the start bit, which is a single high bit
+    bool mLastBit = true;
+    size_t mBitsInSequence = 1;
+};
+
 void ToyotaBeanAnalyzer::WorkerThread()
 {
     mSampleRateHz = GetSampleRate();
 
     mSerial = GetAnalyzerChannelData(mSettings->mInputChannel);
 
-    if (mSerial->GetBitState() == BIT_LOW)
-        mSerial->AdvanceToNextEdge();
-
     U32 samples_per_bit = mSampleRateHz / mSettings->mBitRate;
-    U32 samples_to_first_center_of_first_data_bit = U32(1.5 * double(mSampleRateHz) / double(mSettings->mBitRate));
+    U32 samples_to_center_of_first_data_bit = U32(1.5 * double(mSampleRateHz) / double(mSettings->mBitRate));
+    // 1 - High start bit
+    // 4 - priority bits
+    // 4 - message length bits (ID + DATA)
+    // 8 - DST-ID destination ID
+    // 8 - MES-ID message ID
+    // 8*1-11 - 1-11 data bytes
+    // 8 - CRC error check
+    // 8 - EOM end of frame
+    // 2 - RSP response (?)
+    // 6 - EOF
 
-    for (;;)
+    while (true)
     {
-        U8 data = 0;
-        U8 mask = 1 << 7;
+        // Find the first rising edge
+        while (mSerial->GetBitState() != BIT_HIGH)
+            mSerial->AdvanceToNextEdge();
 
-        mSerial->AdvanceToNextEdge(); // falling edge -- beginning of the start bit
+        auto frameStartSample = mSerial->GetSampleNumber();
 
-        U64 starting_sample = mSerial->GetSampleNumber();
+        // Advance to the center of the first bit after the start bit
+        mSerial->Advance(samples_to_center_of_first_data_bit);
 
-        mSerial->Advance(samples_to_first_center_of_first_data_bit);
+        BitUnstuffer bits(mSerial, mResults, mSettings->mInputChannel, samples_per_bit);
 
-        for (U32 i = 0; i < 8; i++)
+        // Priority
+        uint8_t pri = bits.ReadBits(4);
+        uint8_t ml = bits.ReadBits(4);
+        uint8_t dstId = bits.ReadByte();
+        uint8_t mesId = bits.ReadByte();
+        uint8_t data[11];
+
+        // Actual number of data bytes is (ml - 3 for dstId, mesId, crc)
+        _ASSERT(ml > 3 && ml <= 14);
+        size_t dataBytes = ml - 3;
+        for (size_t i = 0; i < dataBytes; i++)
         {
-            // let's put a dot exactly where we sample this bit:
-            mResults->AddMarker(mSerial->GetSampleNumber(), AnalyzerResults::Dot, mSettings->mInputChannel);
-
-            if (mSerial->GetBitState() == BIT_HIGH)
-                data |= mask;
-
-            mSerial->Advance(samples_per_bit);
-
-            mask = mask >> 1;
+            data[i] = bits.ReadByte();
         }
 
-        // we have a byte to save.
-        Frame frame;
-        frame.mData1 = data;
-        frame.mFlags = 0;
-        frame.mStartingSampleInclusive = starting_sample;
-        frame.mEndingSampleInclusive = mSerial->GetSampleNumber();
+        uint8_t crc = bits.ReadByte();
 
-        mResults->AddFrame(frame);
+        // TODO: is there an always-zero bit here?
+
+        uint8_t rsp = bits.ReadBits(2);
+        uint8_t eom = bits.ReadBits(8);
+
+        // We have a frame to save
+        FrameV2 frame;
+        frame.AddInteger("PRI", pri);
+        frame.AddInteger("ML", ml);
+        frame.AddInteger("DST-ID", dstId);
+        frame.AddInteger("MES-ID", mesId);
+
+        mResults->AddFrameV2(frame, "bean", frameStartSample, mSerial->GetSampleNumber());
         mResults->CommitResults();
-        ReportProgress(frame.mEndingSampleInclusive);
+        ReportProgress(mSerial->GetSampleNumber());
     }
 }
 
